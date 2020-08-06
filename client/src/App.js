2cae5519cf3c1d2c0f2651a41bc7dd3d67a8e7f6
Austin Keener <austingraphicandweb@gmail.com>
2020-08-06 11:52:15 -0400

    initial file setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+     <h1>Testing out to see whether or not something prints to the webpage when I run this application.</h1>
+    </div>
+  );
+}
+
+export default App;
