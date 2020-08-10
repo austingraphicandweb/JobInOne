d4355b2debff777dc25d1deb78266b3ece94775b
Austin Keener <austingraphicandweb@gmail.com>
2020-08-10 13:06:20 -0400

    building text fields for form

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      '& > *': {
-        margin: theme.spacing(1),
-      },
+        '& > *': {
+            margin: theme.spacing(1),
+        },
     },
-  }));
+}));
 
-  export default function ContainedButtons() {
+export default function ContainedButtons() {
     const classes = useStyles();
-        return (
-            <div className={classes.root}>
-                <h2>Search Jobs</h2>
-                <Button variant="contained">Search</Button>
-            </div>
-        );
-    }
+    return (
+
+        <div className={classes.root}>
+            <h2>Search Jobs</h2>
+            {/**
+             * input form
+             */}
+
+            <form className={classes.root} noValidate autoComplete="off">
+                <TextField id="standard-secondary" label="Job Title" color="secondary" />
+                <TextField id="standard-secondary" label="Position Level" color="secondary" />
+                <TextField id="standard-secondary" label="Zip Code" color="secondary" />
+            </form>
+            {/*End form*/}
+            <Button variant="contained">Search</Button>
+             {/**
+              * show results form.
+              */}
+        </div>
+    );
+}
