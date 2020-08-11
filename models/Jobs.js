f5529ee73cdc2a5f41830874b3c57606085b611b
Austin Keener <austingraphicandweb@gmail.com>
2020-08-11 11:13:28 -0400

    information added to the jobs schema

--- models/Jobs.js
+++ models/Jobs.js
@@ -2,7 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const JobSearch = new Schema({
-
+    job_title: {
+        type: String,
+        trim: true,
+        required: 'The job title is required within the search.'
+    },
+    position_level: {
+        type: String,
+        trim: true,
+        required: 'You need to specify what level of position you are looking for.'
+    },
+    zip_code: {
+        type: Int,
+        required: 'Please enter in a valid zip code.'
+    }
 });
 
 const Job = mongoose.model('Job',JobSearch);
