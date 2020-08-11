c54691ff5f6e296a7a83c54cdcfa170bb3ab8fb8
Austin Keener <austingraphicandweb@gmail.com>
2020-08-11 11:20:03 -0400

    setting up jobs controller

---
+++ controllers/jobsController.jsx
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
+// READ ALL
+
+// READ ONE
+
+// CREATE
+
+//EDIT
+
+//DELETE
+
+module.exports = router;
