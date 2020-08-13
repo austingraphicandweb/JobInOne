96855e8dd6d5939b597aa6e7a93d8bd27d8abe9d
Austin Keener <austingraphicandweb@gmail.com>
2020-08-13 12:17:33 -0400

    navigation bar functionality fixed

--- client/src/components/Nav/nav.jsx
+++ client/src/components/Nav/nav.jsx
@@ -1,56 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`nav-tabpanel-${index}`}
-      aria-labelledby={`nav-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `nav-tab-${index}`,
-    'aria-controls': `nav-tabpanel-${index}`,
-  };
-}
-
-function LinkTab(props) {
-  return (
-    <Tab
-      component="a"
-      onClick={(event) => {
-        event.preventDefault();
-      }}
-      {...props}
-    />
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -76,10 +29,10 @@ export default function NavTabs() {
           onChange={handleChange}
           aria-label="nav tabs example"
         >
-          <LinkTab label="Home" href="/Home" {...a11yProps(0)} />
-          <LinkTab label="Add" href="/Add" {...a11yProps(0)} />
-          <LinkTab label="Saves" href="/Saves" {...a11yProps(1)} />
-          <LinkTab label="Create Account" href="/create" {...a11yProps(0)} />
+          <Tab label="Home" component={Link} to="/" />
+          <Tab label="Add" component={Link} to="/Add" />
+          <Tab label="Saves" component={Link} to="/Saves" />
+          <Tab label="Create Account" component={Link} to="/create" />
         </Tabs>
       </AppBar>
     </div>
