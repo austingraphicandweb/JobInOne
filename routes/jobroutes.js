0297c7ed5f7bbf37fe9cccd03464d5b038693dcf
Austin Keener <austingraphicandweb@gmail.com>
2020-08-13 19:10:19 -0400

    update job route created

---
+++ routes/jobroutes.js
@@ -0,0 +1,77 @@
+const Job = require('../models/Jobs');
+
+createJob = (req, res) => {
+    const body = req.body
+
+    if(!body) {
+        return res.status(400).json({
+            success: false,
+            error: "please fill out all the required fields"
+        })
+    }
+
+const job = new Job(body)
+
+if (!movie){
+    return res.status(400).json({
+        success:false,
+        error:err
+    })
+}
+
+job
+    .save()
+    .then(() => {
+        return res.status(201).json({
+            success:true,
+            id:job._id,
+            message:'job created!!'
+        })
+    }) .catch(error => {
+        return res.status(400).json({
+            error,
+            message:'job not created :('
+        })
+    })
+}
+
+updateJob = async (req,res) => {
+    const body = req.body
+
+    if(!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'Please specify which job you want to update'
+        })
+    }
+
+    Job.findOne(
+        {_id:req.params.id}, (err,movie) => {
+            if (err) {
+                return res.status(404).json({
+                    err,
+                    message:'Job not found',
+                })
+            }
+            job.job_title = body.job_title
+            job.company = body.company
+            job.url = body.url
+            job.date_found = body.date_found
+            job
+                .save()
+                .then(() => {
+                    return res.status(200).json({
+                        success:true,
+                        id:job._id,
+                        message:'job is updated!!',
+                    })
+                })
+                .catch(error => {
+                    return res.status(404).json({
+                        error,
+                        message:'job not updated...',
+                    })
+                })
+        })
+}
+
