d0d75d29a1f3194882f110190d6114eaa2c263e3
Austin Keener <austingraphicandweb@gmail.com>
2020-08-12 20:47:24 -0400

    jobscontroller info replaces with info on server page

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,37 +1,43 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../models');
-const { replaceOne } = require('../models/Jobs');
+// const express = require('express');
+// const router = express.Router();
+// const db = require('../models');
+// const { replaceOne } = require('../models/Jobs');
+// const app = express();
+// app.use(express.static("dist"));
+// // READ ALL
+// app.get('/jobs', (req,res) => {
+//     db.Job.find({}).then(foundJobs => {
+//         res.json({
+//             error:false,
+//             data:foundJobs,
+//             message:'Successfully retrieved jobs from the source.'
+//         }).catch((err) => {
+//             res.status(500).json({
+//                 error:true,
+//                 data:null,
+//                 message:'Unable to get the jobs from the job source.'
+//             })
+//         })
+//     })
+//     console.log("hello world!");
+//     res.send('stringggg');
+// })
 
-// READ ALL
-router.get('/jobs', (req,res) => {
-    db.Job.find({}).then(foundJobs => {
-        res.json({
-            error:false,
-            data:foundJobs,
-            message:'Successfully retrieved jobs from the source.'
-        }).catch((err) => {
-            res.status(500).json({
-                error:true,
-                data:null,
-                message:'Unable to get the jobs from the job source.'
-            })
-        })
-    })
-})
+// router.get('/jobs/:name', (req,res) => {
+//     console.log(req.params.name)
+//     db.Job.find({
+//         job_title:req.params.name
+//     })
+// })
+// // READ ONE
 
-router.get('/jobs/:name', (req,res) => {
-    console.log(req.params.name)
-    db.Job.find({
-        job_title:req.params.name
-    })
-})
-// READ ONE
+// // CREATE
+// app.post('/jobs', (req,res) => {
+//     res.send(req.body);
+// })
 
-// CREATE
+// //EDIT
 
-//EDIT
+// //DELETE
 
-//DELETE
-
-module.exports = router;
+// module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 const session = require("express-session");
 const jobsController = require('./controllers/jobsController');
 const userController = require('./controllers/userController');
+const Job = require("./models/Jobs");
 const passport = require("./passport/index");
+const { Recoverable } = require("repl");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
@@ -37,6 +39,22 @@ app.use("/api", userController);
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+app.post("/jobs", (req,res) => {
+  const jobPost = new Job({
+    job_title: req.body.job_title,
+    company: req.body.company,
+    url: req.body.url,
+    date_found: req.body.date_found
+  });
+  jobPost.save((err) => {
+    if (err){
+      res.send("This did not post and you need to retry.");
+    } else {
+      res.send("Post successful");
+    }
+  })
+})
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
