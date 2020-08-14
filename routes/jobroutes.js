20e8de9cd898f69f8446ae992a3b0af02714789a
Austin Keener <austingraphicandweb@gmail.com>
2020-08-14 11:55:15 -0400

    deleted routes folder

--- routes/jobroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const JobCtrl = require('../controllers/jobcontroller');
-const router = express.Router();
-
-// router.put('/job/:id', JobCtrl.updateJob)
-// router.delete('/movie/:id', JobCtrl.deleteJob)
-// router.get('/job', JobCtrl.getJobs)
-
-module.exports = router;
