89652ff51958ee0e4d8bf8f56ee1409a59047499
Austin Keener <austingraphicandweb@gmail.com>
2020-08-14 20:21:28 -0400

    basic crud finished

--- client/src/components/jobsTable/jobsTable.jsx
+++ client/src/components/jobsTable/jobsTable.jsx
@@ -28,11 +28,19 @@ function JobsTable({ jobs, jobsSorted, onDelete, onUpdate }){
                         </td>
                         <td>
                         {!editable?<span onClick={() => setEditable(true)}>{job.company}</span>:(
-                          <input placeholder={job.company} onBlur={(ev) => onUpdate(job._id,'job_title',ev.target.value)} />  
+                          <input placeholder={job.company} onBlur={(ev) => onUpdate(job._id,'company',ev.target.value)} />  
                         )}
                         </td>
-                        <td>{job.url}</td>
-                        <td>{job.date_found}</td>
+                        <td>
+                        {!editable?<span onClick={() => setEditable(true)}>{job.url}</span>:(
+                          <input placeholder={job.url} onBlur={(ev) => onUpdate(job._id,'url',ev.target.value)} />  
+                        )}
+                        </td>
+                        <td>
+                        {!editable?<span onClick={() => setEditable(true)}>{job.date_found}</span>:(
+                          <input placeholder={job.date_found} onBlur={(ev) => onUpdate(job._id,'date_found',ev.target.value)} />  
+                        )}
+                        </td>
                         <td><FontAwesomeIcon icon={faTrashAlt} onClick={() => onDelete(job._id)} /></td>
                     </tr>
                 ))}
