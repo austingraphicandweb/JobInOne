f6a4369bbf635734d58750afab4209f47bba7dbb
Austin Keener <austingraphicandweb@gmail.com>
2020-08-16 01:28:51 -0400

    added buttons to home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -6,10 +6,10 @@ function Home() {
         <div className="homeBackground">
             <h1 className="text head">JobInOne</h1>
             <h3 className="text subhead"><i>Where will you go next?</i></h3>
-            {/* <div className="buttons">
+            <div className="buttons">
                 <button href="/add" className="btn">Start adding jobs</button>
                 <button href="/saves" className="btn">View previously added jobs</button>
-            </div> */}
+            </div>
 
         </div>
     );
