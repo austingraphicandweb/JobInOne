dcba1920be4f036ce51153dd3f0f88a9b5a105c0
Austin Keener <austingraphicandweb@gmail.com>
2020-08-15 20:11:45 -0400

    cleaning up server file

--- server.js
+++ server.js
@@ -1,41 +1,44 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const path = require("path");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const session = require("express-session");
-const passport = require("./passport/index");
+// const session = require("express-session");
+// const passport = require("./passport/index");
 const JobController = require('./controllers/jobcontroller');
 const UserController = require('./controllers/userController');
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(
-	session({
-		secret: 'hi', //pick a random string to make the hash that is generated secure
-		resave: false, //required
-		saveUninitialized: false //required
-	})
-)
-app.use(passport.initialize())
-app.use(passport.session())
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use(
+// 	session({
+// 		secret: 'hi', //pick a random string to make the hash that is generated secure
+// 		resave: false, //required
+// 		saveUninitialized: false //required
+// 	})
+// )
+// app.use(passport.initialize())
+// app.use(passport.session())
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+// }
 // Connect to the Mongo DB
 mongoose.Promise=global.Promise;
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://heroku_6mb711vl:.4Mg3ZXfVm#v3SQ@ds061676.mlab.com:61676/heroku_6mb711vl",
-  { useMongoClient:true}
+  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 // Send every request to the React app
 // Define any API routes before this runs
 app.use(JobController);
 app.use(UserController);
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
