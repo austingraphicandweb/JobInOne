e37a6b32066438fe874d3e40b1428221702a2200
Austin Keener <austingraphicandweb@gmail.com>
2020-08-16 15:38:14 -0400

    installing path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const path = require('path')
 // const path = require("path");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
