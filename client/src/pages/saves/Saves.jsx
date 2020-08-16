93625269e0bf1d47fb308b0e4cf80589fd6d06eb
Austin Keener <austingraphicandweb@gmail.com>
2020-08-16 14:54:55 -0400

    fixing getJobs route in saves page

--- client/src/pages/saves/Saves.jsx
+++ client/src/pages/saves/Saves.jsx
@@ -12,20 +12,17 @@ class JobsList extends Component {
     componentDidMount(){
         this.getJobs();
     }
-    // getJobs = () => {
-    //     axios 
-    //         .get('/job')
-    //         .then((response) => {
-    //             this.setState({
-    //                 jobs: response.data.data,
-    //             })
-    //         }).catch((err) => {
-    //             console.log(err)
-    //         })
-    // }
 
     getJobs = () => {
-        location.reload();
+        axios 
+            .get('/job')
+            .then((response) => {
+                this.setState({
+                    jobs: response.data.data,
+                })
+            }).catch((err) => {
+                console.log(err)
+            })
     }
 
     deleteJob = (id) => {
