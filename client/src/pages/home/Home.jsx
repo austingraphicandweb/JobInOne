4f9dd5037906d32a2b5b1647e60d93de0ad14686
Austin Keener <austingraphicandweb@gmail.com>
2020-08-16 01:23:15 -0400

    home page styling

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,6 +4,8 @@ import './home.css';
 function Home() {
     return (
         <div className="homeBackground">
+            <h1 className="text head">JobInOne</h1>
+            <h3 className="text subhead"><i>Where will you go next?</i></h3>
             {/* <div className="buttons">
                 <button href="/add" className="btn">Start adding jobs</button>
                 <button href="/saves" className="btn">View previously added jobs</button>
